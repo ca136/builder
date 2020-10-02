2ec247e8a6656a45b3a2b38021e5c51ad3f8b2dc
Caleb Amsden <caleb.amsden@gmail.com>
2020-10-02 13:55:14 -0700

    remove logs

--- examples/storybook/src/components/HeroWithChildren/HeroWithChildren.stories.jsx
+++ examples/storybook/src/components/HeroWithChildren/HeroWithChildren.stories.jsx
@@ -4,7 +4,6 @@ import { getDefaultProps } from '@builder.io/storybook';
 import { HeroWithBuilderChildrenConfig, HeroWithBuilderChildren } from './HeroWithChildren.builder';
 
 const props = getDefaultProps(HeroWithBuilderChildrenConfig);
-console.log('foooofds', props);
 
 export default {
   title: 'Hero with Children',
